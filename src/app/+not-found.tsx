7c233e9ea3307bed0d2b0be07f88c6aa453d1ee3
Collins N <collinsn908@gmail.com>
2026-04-15 16:04:41 -0700

    Checkpoint Save

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -1,2 +1,9 @@
 // +not-found.tsx
 // This route will be displayed if user navigates to a route that doesn't exist
+
+import { Text } from 'react-native';
+export default function NotFound() {
+    return(
+        <Text>If this page pops up, something was not found </Text>
+    );
+}
---
+++ src/lib/supabase.ts
@@ -0,0 +1,14 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://pscxiosdaxdxirpeuluv.supabase.co'
+const supabasePublishableKey = 'sb_publishable_eaPrN37MT15RKU-vuzoWOw_SmMgBugl'
+
+export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
